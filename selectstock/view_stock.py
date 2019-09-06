405fb0abab5f831e1a277d7a02d2d56f2d34a125
xiangnan at office <49220598@qq.com>
2019-09-06 17:03:53 +0800

    reconstruct choosestocks module, generate excel according to the given industry code.

---
+++ selectstock/view_stock.py
@@ -0,0 +1,11 @@
+from jqdatasdk import *
+from selectstock.choosestocks import generate_stock_info
+
+auth('13578992860', '1qaz2wsx')
+
+# C15 酒、饮料和精制茶制造业
+# C27 药
+# C36 汽车制造业
+# generate_stock_info('C15')
+# generate_stock_info('C27')
+generate_stock_info('C36')
